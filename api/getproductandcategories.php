794944dca653a5e733fe3bd9c67e48b5743cdbc4
Quyet Chu <ftewman@gmail.com>
2016-12-17 00:45:54 +0700

    update get produce and categories
    done

--- api/getproductandcategories.php
+++ api/getproductandcategories.php
@@ -1,15 +1,24 @@
 	<?php
 	include 'db.php';
 	
-	$queryCat = 'SELECT * FROM `tbl_loaisanpham`';
-	$resultCat = $conn->query ( $queryCat );
-	
 	$queryP = "SELECT * FROM `tbl_sanpham`";
 	$resultP = $conn->query ( $queryP );
+	
+	$queryCat = 'SELECT * FROM `tbl_loaisanpham`';
+	$resultCat = $conn->query ( $queryCat );
 	
 	$arrS = array ();
 	$arrP = array ();
 	$arrCat = array ();
+	
+	
+	if ($resultP->num_rows > 0) {
+		while ( $rowP = $resultP->fetch_assoc () ) {
+			array_push ( $arrP, $rowP );
+		}
+		$arrS ["Products"] = $arrP;
+		// echo json_encode ( $arrP );
+	}
 	
 	if ($resultCat->num_rows > 0) {
 		while ( $rowCat = $resultCat->fetch_assoc () ) {
@@ -19,13 +28,6 @@
 		// echo json_encode ( $arrP );
 	}
 	
-	if ($resultP->num_rows > 0) {
-		while ( $rowP = $resultP->fetch_assoc () ) {
-			array_push ( $arrP, $rowP );
-		}
-		$arrS ["Products"] = $arrP;
-		// echo json_encode ( $arrP );
-	}
 	echo json_encode ( $arrS );
 	
 	$conn->close ();
